cd97d60dd110683c5692796553666d5cdf0b05c3
Denis <den.ostroukhov.02@mail.ru>
2022-01-24 02:19:35 +0700

    1.1 version

--- WinFormsKP/Form1.cs
+++ WinFormsKP/Form1.cs
@@ -12,13 +12,11 @@ namespace WinFormsKP
 {
     public partial class Form1 : Form
     {
-        int ID = 0;
-        static int Num;
+        static int ID = 0;
         Patient Patient1 = new Patient();
         Doctor[] DoctorMas = new Doctor[10];
         Doctor DoctorSelect = new Doctor();
         static Question[] Question1 = new Question[2];
-        Form2 form2 = new Form2(Question1, Num);
         public Form1()
         {
             InitializeComponent();
@@ -27,7 +25,7 @@ namespace WinFormsKP
         private void Form1_Load(object sender, EventArgs e)
         {
             DoctorMas[0] = new Doctor("Сергеев", "Юрий", "Владимирович", "Терапевт");
-            DoctorMas[1] = new Doctor("Буянов", "Виталий", "Юрьевич", "Проктолог");
+            DoctorMas[1] = new Doctor("Буянов", "Виталий", "Юрьевич", "Психиатр");
             
             this.ComboBoxSelectDoctor.Items.AddRange(new string[] { DoctorMas[0].GetSpecialization() + ": " + DoctorMas[0].GetSurname() + " " + DoctorMas[0].GetName() + " " + DoctorMas[0].GetPatronymic(), DoctorMas[1].GetSpecialization() + ": " + DoctorMas[1].GetSurname() + " " + DoctorMas[1].GetName() + " " + DoctorMas[1].GetPatronymic() });
         }
@@ -57,145 +55,10 @@ namespace WinFormsKP
             {
                 Patient1.Set(TextBoxSurnamePatient.Text, TextBoxNamePatient.Text, TextBoxPatronymicPatient.Text);
                 QuestionPool();
-                switch (ID)
-                { 
-                    case 0:
-                        {
-                            Question1[0].SetDoctor(DoctorSelect);
-                            Question1[0].SetPatient(Patient1);
-                            if (Question1[0].GetQuestions(0) != "")
-                            {
-                                form2.groupBox1.Visible = true;
-                                form2.label1.Visible = true;
-                                form2.label1.Text = Question1[0].GetQuestions(0);
-                            }
-                            if (Question1[0].GetQuestions(1) != "")
-                            {
-                                form2.groupBox2.Visible = true;
-                                form2.label2.Visible = true;
-                                form2.label2.Text = Question1[0].GetQuestions(1);
-                            }
-                            if (Question1[0].GetQuestions(2) != "")
-                            {
-                                form2.groupBox3.Visible = true;
-                                form2.label3.Visible = true;
-                                form2.label3.Text = Question1[0].GetQuestions(2);
-                            }
-                            if (Question1[0].GetQuestions(3) != "")
-                            {
-                                form2.groupBox4.Visible = true;
-                                form2.label4.Visible = true;
-                                form2.label4.Text = Question1[0].GetQuestions(3);
-                            }
-                            if (Question1[0].GetQuestions(4) != "")
-                            {
-                                form2.groupBox5.Visible = true;
-                                form2.label5.Visible = true;
-                                form2.label5.Text = Question1[0].GetQuestions(4);
-                            }
-                            if (Question1[0].GetQuestions(5) != "")
-                            {
-                                form2.groupBox6.Visible = true;
-                                form2.label6.Visible = true;
-                                form2.label6.Text = Question1[0].GetQuestions(5);
-                            }
-                            if (Question1[0].GetQuestions(6) != "")
-                            {
-                                form2.groupBox7.Visible = true;
-                                form2.label7.Visible = true;
-                                form2.label7.Text = Question1[0].GetQuestions(6);
-                            }
-                            if (Question1[0].GetQuestions(7) != "")
-                            {
-                                form2.groupBox8.Visible = true;
-                                form2.label8.Visible = true;
-                                form2.label8.Text = Question1[0].GetQuestions(7);
-                            }
-                            if (Question1[0].GetQuestions(8) != "")
-                            {
-                                form2.groupBox9.Visible = true;
-                                form2.label9.Visible = true;
-                                form2.label9.Text = Question1[0].GetQuestions(8);
-                            }
-                            if (Question1[0].GetQuestions(9) != "")
-                            {
-                                form2.groupBox10.Visible = true;
-                                form2.label10.Visible = true;
-                                form2.label10.Text = Question1[0].GetQuestions(9);
-                            }
-                            Num = 0;
-                            form2.Show();
-                            break;
-                        }
-                    case 1:
-                        {
-                            Question1[1].SetDoctor(DoctorSelect);
-                            Question1[1].SetPatient(Patient1);
-                            if (Question1[1].GetQuestions(0) != "")
-                            {
-                                form2.groupBox1.Visible = true;
-                                form2.label1.Visible = true;
-                                form2.label1.Text = Question1[1].GetQuestions(0);
-                            }
-                            if (Question1[1].GetQuestions(1) != "")
-                            {
-                                form2.groupBox2.Visible = true;
-                                form2.label2.Visible = true;
-                                form2.label2.Text = Question1[1].GetQuestions(1);
-                            }
-                            if (Question1[1].GetQuestions(2) != "")
-                            {
-                                form2.groupBox3.Visible = true;
-                                form2.label3.Visible = true;
-                                form2.label3.Text = Question1[1].GetQuestions(2);
-                            }
-                            if (Question1[1].GetQuestions(3) != "")
-                            {
-                                form2.groupBox4.Visible = true;
-                                form2.label4.Visible = true;
-                                form2.label4.Text = Question1[1].GetQuestions(3);
-                            }
-                            if (Question1[1].GetQuestions(4) != "")
-                            {
-                                form2.groupBox5.Visible = true;
-                                form2.label5.Visible = true;
-                                form2.label5.Text = Question1[1].GetQuestions(4);
-                            }
-                            if (Question1[1].GetQuestions(5) != "")
-                            {
-                                form2.groupBox6.Visible = true;
-                                form2.label6.Visible = true;
-                                form2.label6.Text = Question1[1].GetQuestions(5);
-                            }
-                            if (Question1[1].GetQuestions(6) != "")
-                            {
-                                form2.groupBox7.Visible = true;
-                                form2.label7.Visible = true;
-                                form2.label7.Text = Question1[1].GetQuestions(6);
-                            }
-                            if (Question1[1].GetQuestions(7) != "")
-                            {
-                                form2.groupBox8.Visible = true;
-                                form2.label8.Visible = true;
-                                form2.label8.Text = Question1[1].GetQuestions(7);
-                            }
-                            if (Question1[1].GetQuestions(8) != "")
-                            {
-                                form2.groupBox9.Visible = true;
-                                form2.label9.Visible = true;
-                                form2.label9.Text = Question1[1].GetQuestions(8);
-                            }
-                            if (Question1[1].GetQuestions(9) != "")
-                            {
-                                form2.groupBox10.Visible = true;
-                                form2.label10.Visible = true;
-                                form2.label10.Text = Question1[1].GetQuestions(9);
-                            }
-                            Num = 1;
-                            form2.Show();
-                            break;
-                        }
-                }
+                Question1[ID].SetDoctor(DoctorSelect);
+                Question1[ID].SetPatient(Patient1);
+                Form2 form2 = new Form2(Question1, ID);
+                form2.Show(); 
             }
         }
         public void QuestionPool()
@@ -206,16 +69,16 @@ namespace WinFormsKP
                 QuestionMas1[i] = "";
             }
 
-            QuestionMas1[0] = "Я терапевт";
-            QuestionMas1[1] = "2";
-            QuestionMas1[2] = "3";
-            QuestionMas1[3] = "4";
-            QuestionMas1[4] = "5";
-            QuestionMas1[5] = "6";
-            QuestionMas1[6] = "7";
-            QuestionMas1[7] = "8";
-            QuestionMas1[8] = "9";
-            QuestionMas1[9] = "10";
+            QuestionMas1[0] = "Вы пьете достаточное количество чистой воды?";
+            QuestionMas1[1] = "Бывают ли у вас перепады настроения?";
+            QuestionMas1[2] = "Вы можете отметить ухудшение самочувствия за последние месяцы/годы?";
+            QuestionMas1[3] = "У вас есть режим питания?";
+            QuestionMas1[4] = "Вы контролируете уровень потребляемого сахара?";
+            QuestionMas1[5] = "Вы пренебрегаете питательными завтраками для себя?";
+            QuestionMas1[6] = "Вы поддерживаете физическую активность?";
+            QuestionMas1[7] = "Вы предпочитаете проводить время пассивно?";
+            QuestionMas1[8] = "Вы сдаете анализы и следите за уровнем гормонов, витаминов, минералов и т.д.?";
+            QuestionMas1[9] = "Вы спите в проветриваемом помещении?";
 
             string[] QuestionMas2 = new string[10];
             for (int i = 0; i < 10; i++)
@@ -223,16 +86,16 @@ namespace WinFormsKP
                 QuestionMas2[i] = "";
             }
 
-            QuestionMas2[0] = "Я проктолог";
-            QuestionMas2[1] = "2";
-            QuestionMas2[2] = "3";
-            QuestionMas2[3] = "4";
-            QuestionMas2[4] = "5";
-            QuestionMas2[5] = "6";
-            QuestionMas2[6] = "7";
-            QuestionMas2[7] = "8";
-            QuestionMas2[8] = "9";
-            QuestionMas2[9] = "10";
+            QuestionMas2[0] = "Бывают ли у вас перепады настроения?";
+            QuestionMas2[1] = "Вы испытываете чувство удовлетворенности собственной жизнью?";
+            QuestionMas2[2] = "У вас есть нереализованные желания и амбиции, подавляющие вас?";
+            QuestionMas2[3] = "У вас есть достижения и успехи, которыми вы можете гордиться?";
+            QuestionMas2[4] = "Самочувствие мешает вам в достижении целей?";
+            QuestionMas2[5] = "Вы часто испытываете неуверенность в себе при общении с другими людьми?";
+            QuestionMas2[6] = "Чувствуете ли вы прилив сил после пробуждения?";
+            QuestionMas2[7] = "У вас есть хобби или ритуалы, наполняющие вас энергией?";
+            //QuestionMas2[8] = "9";
+            //QuestionMas2[9] = "10";
 
             Question1[0] = new Question();
             Question1[1] = new Question();
--- WinFormsKP/Form2.cs
+++ WinFormsKP/Form2.cs
@@ -29,6 +29,66 @@ namespace WinFormsKP
         {
             labelDoctor.Text = QuestionForm2[index].GetDoctor().Info();
             labelPatient.Text = QuestionForm2[index].GetPatient().Info();
+            if (QuestionForm2[index].GetQuestions(0) != "")
+            {
+                this.groupBox1.Visible = true;
+                this.label1.Visible = true;
+                this.label1.Text = QuestionForm2[index].GetQuestions(0);
+            }
+            if (QuestionForm2[index].GetQuestions(1) != "")
+            {
+                this.groupBox2.Visible = true;
+                this.label2.Visible = true;
+                this.label2.Text = QuestionForm2[index].GetQuestions(1);
+            }
+            if (QuestionForm2[index].GetQuestions(2) != "")
+            {
+                this.groupBox3.Visible = true;
+                this.label3.Visible = true;
+                this.label3.Text = QuestionForm2[index].GetQuestions(2);
+            }
+            if (QuestionForm2[index].GetQuestions(3) != "")
+            {
+                this.groupBox4.Visible = true;
+                this.label4.Visible = true;
+                this.label4.Text = QuestionForm2[index].GetQuestions(3);
+            }
+            if (QuestionForm2[index].GetQuestions(4) != "")
+            {
+                this.groupBox5.Visible = true;
+                this.label5.Visible = true;
+                this.label5.Text = QuestionForm2[index].GetQuestions(4);
+            }
+            if (QuestionForm2[index].GetQuestions(5) != "")
+            {
+                this.groupBox6.Visible = true;
+                this.label6.Visible = true;
+                this.label6.Text = QuestionForm2[index].GetQuestions(5);
+            }
+            if (QuestionForm2[index].GetQuestions(6) != "")
+            {
+                this.groupBox7.Visible = true;
+                this.label7.Visible = true;
+                this.label7.Text = QuestionForm2[index].GetQuestions(6);
+            }
+            if (QuestionForm2[index].GetQuestions(7) != "")
+            {
+                this.groupBox8.Visible = true;
+                this.label8.Visible = true;
+                this.label8.Text = QuestionForm2[index].GetQuestions(7);
+            }
+            if (QuestionForm2[index].GetQuestions(8) != "")
+            {
+                this.groupBox9.Visible = true;
+                this.label9.Visible = true;
+                this.label9.Text = QuestionForm2[index].GetQuestions(8);
+            }
+            if (QuestionForm2[index].GetQuestions(9) != "")
+            {
+                this.groupBox10.Visible = true;
+                this.label10.Visible = true;
+                this.label10.Text = QuestionForm2[index].GetQuestions(9);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
